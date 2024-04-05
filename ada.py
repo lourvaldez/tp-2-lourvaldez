07dad7fed3919fa5122fef0c83a827b1bb088c28
lourvaldez <162723167+lourvaldez@users.noreply.github.com>
2024-04-04 21:37:13 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    fullname= "      "+first_name+" "+last_name
+    print(fullname.lower().strip())
+    print(fullname.title().strip())
+    print(fullname.upper().strip())
+    print(fullname.lower())
+ada()   
