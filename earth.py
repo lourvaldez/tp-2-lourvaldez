69530a52faa37a4d9e463f0e7336e2935fb9eb89
lourvaldez <162723167+lourvaldez@users.noreply.github.com>
2024-04-04 21:40:26 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,11 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+     y = "Barbados"
+    t= x<y
+    f= y<x 
+    frase1= f"The result of X comes first in the dictionary than Y is {t}"
+    frase2= f"The result of Y comes first in the dictionary than X is {f}"
+    print(frase1)
+    print(frase2)
+earth()
